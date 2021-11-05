fcc53d2f86db6b848c42bb8487dd199313d5c940
Atixcarlos <atix.figueroa@gmail.com>
2021-11-05 10:28:51 -0500

    first commit

---
+++ alternative_script.js
@@ -0,0 +1,89 @@
+var map = L.map('map').setView([-10.406105052399276,-147.5690460205078], 2);
+
+// add Leaflet-Geoman controls with some options to the map  
+map.pm.addControls({  
+  position: 'topleft',  
+drawMarker: false,
+        drawPolygon: false,
+        drawPolyline: false,
+        drawCircle: false,
+        editPolygon: false,
+        deleteLayer: false,
+        drawRectangle: false,
+        drawCircleMarker: false,
+        editControls: false,
+});  
+
+
+map.pm.setGlobalOptions({ snappable: true,allowEditing:false,
+ }); 
+var MyCustomMarker = L.Icon.extend({
+options: {
+    shadowUrl: null,
+    iconAnchor: new L.Point(12, 12),
+    iconSize: new L.Point(24, 10),
+    iconUrl: 'window-small.png'
+}
+});
+/*
+let myButton = new L.Control.PMButton({
+        actions: ["cancel"],
+        onClick: () => { new L.Marker(map, { icon: new MyCustomMarker() })},
+        afterClick: () => {},
+        doToggle: true,
+        toggleStatus: false,
+        disableOtherButtons: true
+    });
+    
+map.addControl(myButton);
+*/
+/*
+ name: 'editgroup',
+            title: 'Edit Plotted boundary',
+            className: 'control-icon leaflet-pm-icon-edit',
+         
+            afterClick: (e, obj) => {
+                if (obj.button._button.toggleStatus) {
+                    drawLayerGroup.pm.enable();
+                } else {
+                    drawLayerGroup.pm.disable();
+                }
+            },
+            doToggle: true,
+            toggleStatus: false,
+            disableOtherButtons: true,
+            block: 'edit',
+            actions: ['finishMode'],
+*/
+function onclick(e){
+    console.log('CLICK');
+  
+}
+
+// add a new custom control
+map.pm.Toolbar.createCustomControl({
+  name: 'CustomButton',
+  block: 'draw',
+  title: 'Small Windows',
+  className: 'control-icon leaflet-pm-icon-small-window',
+
+
+   onClick: () => { 
+
+
+
+},
+  actions: [{
+    text: 'Cancel', 
+    onClick: onclick 
+  }]
+})
+
+var drawnItems = new L.FeatureGroup();
+map.addLayer(drawnItems);
+
+
+
+
+var geojson = L.geoJson(myGeoJson, {}).addTo(drawnItems);
+
---
+++ script.js
@@ -0,0 +1,81 @@
+var map = L.map('map');
+
+map.pm.setGlobalOptions({ snappable: true, allowEditing: false,
+ }); 
+
+// Adding geojson layer (Walls)
+var drawnItems = new L.FeatureGroup();
+map.addLayer(drawnItems);
+var geojson = L.geoJson(myGeoJson, {}).addTo(drawnItems);
+
+    map.fitBounds(geojson.getBounds());
+var options = {
+  position: 'topright',
+  drawMarker: false,
+  drawPolygon: false, 
+  drawPolyline: false, 
+  drawCircle: false,
+  drawCircleMarker: false,
+  drawRectangle: false,
+  cutPolygon: false, 
+  dragMode: false,
+  deleteLayer: false,
+  editMode: false,
+  rotateMode: false,
+};
+
+// add leaflet.pm controls to the map
+map.pm.addControls(options);
+
+// listen to when a new layer is created
+map.on('pm:create', function(e) {
+  console.log(e)
+});
+
+
+// Function to create a custom icon
+function createDrawnIcon(id, type) {
+    var iconUrl = [
+      'window-small.png',
+      'window-medium.png',
+      'window-large.png',
+      'door.png'          
+    ];
+
+    var my_icon = L.icon({
+        iconUrl: iconUrl[id],
+        iconAnchor: new L.Point(12, 12),
+        iconSize: new L.Point(24, 24),
+    });
+
+    my_icon.myType = type;
+
+    return(my_icon);
+}
+
+ 
+// Adding Custom controls
+var smallMarker = map.pm.Toolbar.copyDrawControl('drawMarker',{name: "smallMarker",className: 'control-icon leaflet-pm-icon-small-window',title: 'Small Windows',})
+smallMarker.drawInstance.setOptions({ markerStyle: { draggable: true, icon : createDrawnIcon(0,'Small Window')}});
+
+var mediumMarker = map.pm.Toolbar.copyDrawControl('drawMarker',{name: "mediumMarker",className: 'control-icon leaflet-pm-icon-medium-window',title: 'Medium Windows',})
+mediumMarker.drawInstance.setOptions({markerStyle: { draggable: true, icon : createDrawnIcon(1,'Medium Window')}});
+
+var largeMarker = map.pm.Toolbar.copyDrawControl('drawMarker',{name: "largeMarker",className: 'control-icon leaflet-pm-icon-large-window',title: 'Large Windows',})
+largeMarker.drawInstance.setOptions({markerStyle: { draggable: true, icon : createDrawnIcon(2,'Large Window')}});
+
+var doorMarker = map.pm.Toolbar.copyDrawControl('drawMarker',{name: "doorMarker",className: 'control-icon leaflet-pm-icon-door',title: 'Doors',})
+doorMarker.drawInstance.setOptions({markerStyle: { draggable: true, icon : createDrawnIcon(3,'Door')}});
+
+
+// Insert legend
+var legend = L.control({position: 'bottomright'});
+legend.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'info legend');
+    div.innerHTML +='<img alt="legend"  width="120px" src="legend.png"/>';
+    return div;
+};
+legend.addTo(map);
+
+
+
